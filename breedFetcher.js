1f54d1b2a5d8362fd490e132f696cc18ff3bac0b
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-18 00:11:29 +0000

    bleh

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,12 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    const data = JSON.parse(body);
+    callback(error, data[0].description);
+  });
+};
 
-const breedSearch = process.argv[2];
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`;
-
-request(URL, function(error, response, body) {
-  if (error) {
-    console.log("There was an error, check your url");
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed not found.");
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = {
+  fetchBreedDescription
+};
